b389de67e1c83b62625c9672990c57e6fad85a53
Jerry Lin <jerry.lin@curiositychina.com>
2017-09-12 01:00:10 +0800

    better

---
+++ run-in.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+udp_proxy_config=config-in.js udp_proxy_target=1.1.1.1 node run.js
---
+++ run-out.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+udp_proxy_config=config-out.js udp_proxy_target=127.0.0.1 node run.js
